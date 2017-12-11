ff74e79bcd2a4ca031845c0d7ddfa52b92067a06
Victor Shepardson <victor.shepardson@gmail.com>
2017-12-10 21:18:22 -0500

    fiddling

--- feedback2.glsl
+++ feedback2.glsl
@@ -28,30 +28,46 @@ vec4 blur5pt(sampler2D t, vec2 px){
     )/8.;
 }
 
+vec4 blur6pt(vec2 px){
+  const vec3 d = vec3(1.,-1.,0.);
+  return (
+    4.*samp(history_0, px)
+    + 2.*samp(history_1, px)
+    + samp(history_0, px+d.xz)
+    + samp(history_0, px+d.yz)
+    + samp(history_0, px+d.zx)
+    + samp(history_0, px+d.zy)
+    )/10.;
+}
+
 void main() {
   vec2 px = gl_FragCoord.xy;
   vec2 p = px/size;
 
-  vec4 c_in = blur5pt(history_1, px);
-  // px += c_in.rg;
-  // vec4 c_blur = blur4pt(history_0, px);
+  vec4 c_in = blur6pt(px);
 
   vec4 c_acc = cos(2.*pi*c_in);
+  vec2 delta = vec2(0.);
   for(int i=0; i<4; i++){
-    px += sin(pi*(c_acc.rg-c_acc.ba));//(c_acc.rg - c_acc.ba)*vec2(-1.,1.);
-    // c_acc += cos(2.*pi*blur5pt(history_0, px));
-    c_acc += blur5pt(history_0, px);
-    c_acc = 2.*c_acc.gbar;
+    delta += sin(0.5*pi*(c_acc.rg-c_acc.ba));
+    px += delta;
+    c_acc += blur6pt(px);
+    c_acc = 1.*c_acc.gbar;
   }
-  c_acc = sin(c_acc/(abs(c_acc.a)+1.));
-  vec4 c_sv = 0.1*sin(2.*pi*(c_acc.abgr+p.xxxy*vec4(1.,2.,3.,1.)));
+  c_acc = sin(pi*c_acc/(abs(c_acc.a)+1.));
+  vec4 c_sv = 0.01*sin(2.*pi*(c_acc.abgr+p.xxxy*vec4(1.,2.,3.,1.)));
 
-  vec4 c0 = mix(c_in, blur5pt(history_0, px), 0.5);
-  vec4 c1 = (c_sv + c0 + c_acc).argb;
-  for(int i=0;i<4;i++){
-    if (c1[i] > 1. || c1[i] < 0.)
-      c1[i] = 1.-c0.argb[i];
-  }
+  // vec4 c0 = blur6pt(px);
+  vec4 c0 = mix(c_in, blur6pt(px), 0.8);
+  vec4 c1 = fract(c_sv + 1. + .1*c0 + c_acc).argb;
+  // for(int i=0;i<4;i++){
+  //   if (c1[i] > 1.)
+  //     c1[i] = 1.-c0.gbar[i];
+  //   else if(c1[i] < 0.)
+  //     c1[i] = 1.-c0.argb[i];
+  // }
+  // float a = .01;
+  // c1 = fract((1.+2.*a)*c1 - a);
   // vec4 c = mix(c1, c0, pow(2., -c1.a));
   vec4 c = mix(c1, c0, 0.9);
   // c = fract(1.1*c-0.05);
