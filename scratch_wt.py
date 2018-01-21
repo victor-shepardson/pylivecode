451d1fa15f94dd076401280ed9db2c8e1ec9daaa
Victor Shepardson <victor.shepardson@gmail.com>
2018-01-21 18:10:28 -0500

    sonification

---
+++ scratch_wt.py
@@ -0,0 +1,70 @@
+import numpy as np
+from vispy import app
+import pyaudio as pa
+from livecode import Layer
+import IPython
+
+size = 620, 660
+# size = 1366, 720
+
+size = np.array(size)
+
+screen = Layer(size, 'display.glsl')
+feedback = Layer(size, 'feedback2.glsl', n=3)
+filtered = Layer(size, 'filter.glsl', n=2)
+readback = Layer(size//4, 'readback.glsl', n=1, autoread=True)
+
+def draw():
+    filtered(color=feedback)
+    feedback(filtered=filtered)
+
+    readback(color=feedback)
+    # print(readback.cpu[0,0,0])
+
+    screen(color=feedback)
+
+class Window(app.Canvas):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._timer = app.Timer('auto', connect=self.update, start=True)
+        self.show()
+    def on_draw(self, event):
+        screen.resize(window.size)
+        draw()
+        self.title=str(self.fps).encode('ascii')
+
+if __name__ == '__main__':
+    # app.use_app('pyqt5')
+    app.set_interactive()
+
+window = Window('pylivecode', size, keys='interactive')
+window.measure_fps(callback=lambda x: None)
+
+audio = pa.PyAudio()
+def sound(in_data, frame_count, time_info, status):
+    try:
+        rb = readback.cpu.reshape(-1, 4)
+        data = (
+            rb[:frame_count, :2]
+            )*2-1
+    except Exception:
+        data = np.zeros((frame_count,2))
+        # data = np.stack([
+        #     np.sin(np.linspace(0,50*2.*np.pi,frame_count)),
+        #     np.sin(np.linspace(0,150*2.*np.pi,frame_count))
+        #     ],1)
+    return (data, pa.paContinue)
+
+stream = audio.open(
+    format=pa.paFloat32,
+    channels=2,
+    rate=24000,
+    output=True,
+    stream_callback=sound
+)
+stream.start_stream()
+
+# try:
+#     app.run()
+# except KeyboardInterrupt:
+#     pass
